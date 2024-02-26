067abcfac79edc87224964daf748fc6e51a4d8f8
Andrew Lobban <andrewglobban@gmail.com>
2024-02-26 01:15:21 -0600

    Emit post on createPost controller

--- controllers/feed.js
+++ controllers/feed.js
@@ -3,6 +3,8 @@ const path = require('path');
 
 const { validationResult } = require('express-validator');
 
+const io = require('../socket');
+// console.log('io value ', io.getIO());
 const Post = require('../models/post');
 const User = require('../models/user');
 
@@ -55,6 +57,11 @@ exports.createPost = async (req, res, next) => {
     const user = await User.findById(req.userId);
     user.posts.push(post);
     await user.save();
+    io.getIO().emit('post', {
+      action: 'create',
+      post: post,
+      creator: { _id: user._id, name: user.name },
+    });
     res.status(201).json({
       message: 'Post created successfully!',
       post: post,
