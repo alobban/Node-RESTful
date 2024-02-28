d8a620440450d6d064e59ddfa8d42213826ea670
Andrew Lobban <andrewglobban@gmail.com>
2024-02-28 04:56:08 -0600

    Secure response headers with Helmet

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const multer = require('multer');
+const helmet = require('helmet');
 
 const feedRoutes = require('./routes/feed');
 const authRoutes = require('./routes/auth');
@@ -55,6 +56,8 @@ app.use((req, res, next) => {
 
 app.use('/feed', feedRoutes);
 app.use('/auth', authRoutes);
+
+app.use(helmet());
 
 app.use((error, req, res, next) => {
   console.log(error);
